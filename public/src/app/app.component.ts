66cc675e92ae4a4a5ee9668adc45fb5911c5765e
amandademetrio <amandademetrio@gmail.com>
2018-08-12 16:20:50 -0700

    Added final CRUD elements

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -10,9 +10,12 @@ export class AppComponent {
   title = 'Amanda';
   tasks = [];
   show = false;
+  newTask = {};
+  showmore = "Show More"
   constructor(private _httpService: HttpService) { };
 
   ngOnInit(): void {
+    this.newTask = {title: "", description: ""};
   }
 
   getTasks() {
@@ -23,7 +26,44 @@ export class AppComponent {
   }
 
   getTask(task) {
-    task.show = true;
+    if (task.show) {
+      task.show = false;
+      this.showmore = "Show More"
+    }
+    else {
+      task.show = true;
+      this.showmore = "Show Less"
+    }
+  }
+
+  showEdit(task) {
+    task.edit = true;
+  }
+
+  addTask() {
+    let observable = this._httpService.createTask(this.newTask);
+    observable.subscribe(data => {
+      console.log("got data from post back",data)
+      this.newTask = { title: "", description: "" }
+      this.getTasks();
+    })
+  }
+
+  deleteTask(task) {
+    let observable = this._httpService.deleteTask(task._id)
+    observable.subscribe(data => {
+    console.log("Deleted the task")
+    this.getTasks();
+    })
+  }
+
+  editTask(task) {
+    let observable = this._httpService.updateTask(task,task._id);
+    observable.subscribe(data => {
+      console.log("Edited the following task",task)
+      this.getTasks();
+    }
+    )
   }
 
 }
